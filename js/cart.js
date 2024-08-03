7c8c70f9110efc55111ab4268038407cb2a41f7d
McMaldo <maldonado.ignacio.pablo@gmail.com>
2024-08-03 14:33:22 -0300

    FIX #0002

--- js/cart.js
+++ js/cart.js
@@ -1,14 +1,8 @@
 function setCartView(){
   return /*html*/`
   <header>
-    <div class="icon-container">
-      <span class="icon" onclick='setView("home")'>arrow_back</span>
-      <span class="icon" onclick='setView("cart")'>shopping_cart</span>
-    </div>
-    <div class="search">
-      <input class="icon" type="submit" value="search">
-      <input type="search" placeholder="What are you looking for?">
-    </div>
+    <span class="icon" onclick='setView("home")'>arrow_back</span>
+    <h2>My Cart</h2>
   </header>
   <main>
     
